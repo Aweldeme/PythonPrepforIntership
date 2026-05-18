bd73dfda7f245e07110a8bffa2964e9755bb619f
Amanuel Weldemeskel <amanuelweldemeskel2022@gmail.com>
2026-05-18 15:36:04 -0500

    added learning material and completed all the exercises for Chapter 2 of day1

---
+++ week1/day1_Chapter2.py
@@ -0,0 +1,19 @@
+#------------Exercise 2 for chapter 2----------------
+#name = input("What is your name?\n")
+#print("Hello " + name)
+
+#------------Exercise 3 for chapter 2--------------
+
+#hours = input("Enter Hours: ")
+#rate = input("Enter Rate: ")
+#pay = int(hours) * float(rate)
+#print("Pay:", pay)
+
+#------------Exercise 4 for chapter 2--------------
+
+#done on python interpreator simple value predictions of the expressions
+
+#------------Exercise 5 for chapter 2--------------
+celsius = input ("Enter temp in celsius: ")
+fahrenheit = (float(celsius)) + 32
+print("that converts to",fahrenheit,"fahrenheit")
