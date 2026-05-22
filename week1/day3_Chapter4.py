2a426612057f535a4d4def5a38c70058dd6c5351
Amanuel Weldemeskel <amanuelweldemeskel2022@gmail.com>
2026-05-22 09:35:38 -0500

    Edited it out code in day 3 chapter 4 and added some notes for it

--- week1/day3_Chapter4.py
+++ week1/day3_Chapter4.py
@@ -17,22 +17,22 @@
 
 #Exercise 7 for Chapter 4
 
-def computegrade(score) :
-    if score < 0.0 or score > 1.0:
-        return "Bad Score"
-    elif score >= 0.9 :
-        return "A"
-    elif score >= 0.8 :
-        return "B"
-    elif score >= 0.7 :
-        return "C"
-    elif score >= 0.6 :
-        return "D"
-    elif score < 0.6:
-        return "F"
-try:
-    rawScore = float(input("Enter Score: "))
-    grade = computegrade(rawScore)
-    print(grade)
-except:
-    print("Bad score")
+# def computegrade(score) :
+#     if score < 0.0 or score > 1.0:
+#         return "Bad Score"
+#     elif score >= 0.9 :
+#         return "A"
+#     elif score >= 0.8 :
+#         return "B"
+#     elif score >= 0.7 :
+#         return "C"
+#     elif score >= 0.6 :
+#         return "D"
+#     else:
+#         return "F"
+# try:
+#     rawScore = float(input("Enter Score: "))
+#     grade = computegrade(rawScore)
+#     print(grade)
+# except:
+#     print("Bad score")
--- week1/week1_notes.md
+++ week1/week1_notes.md
@@ -1,8 +1,14 @@
 ------------day 1 reflection notes-------------
-
+CH2
 Python concatenation works differently than compared with java. Python uses commas for separation if printing strings and other types in the same print statement.
 To convert input variables to int/float or any other type, conversion is necessary with their respective function (e.g int("Example variable") to convert to int).
 
 ------------day 2 reflection notes-------------
-
+CH3
 Python and Java have similar comparison operators but the operators have slight differences in what they do depending on what they are comparing (primitive values, object, .....)
+
+------------day 3 reflection notes--------------
+CH4
+Function return preferred instead of printing
+Correct use of try except, exception handling
+parameter heavy instead of outside variables (Functions)
