4db8496ca0489301bcfb3f830a10768c721ed663
Amanuel Weldemeskel <amanuelweldemeskel2022@gmail.com>
2026-05-26 20:43:33 -0500

    no new reading just two more exercises

---
+++ week1/day5_Practice.py
@@ -0,0 +1,29 @@
+#factorial function
+
+# def factorial(fac):
+#     result = fac
+#     while True:
+#         result = result * (fac - 1)
+#         fac = fac - 1
+#         if fac == 1:
+#             break
+#     return result
+# number = float(input("Enter a number: "))
+# result = factorial(number)  
+# print(result)
+
+# 10 num script
+# count = 10
+# sum = 0
+# largest = None
+# smallest = None
+# while count > 0:
+#     num = int(input("Enter 10 numbers: "))
+#     if largest is None or largest < num:
+#         largest = num
+#     if smallest is None or smallest > num:
+#         smallest = num
+#     count = count - 1
+#     sum = sum + num
+# average = sum / 10
+# print("largest:",largest,"smallest:",smallest,"average",average)
