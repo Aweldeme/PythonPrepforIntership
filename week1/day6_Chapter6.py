c4553a11930344b20b42f9e847564b1c969149ab
Amanuel Weldemeskel <amanuelweldemeskel2022@gmail.com>
2026-05-31 16:46:32 -0500

    finished chapter 6 exercises

---
+++ week1/day6_Chapter6.py
@@ -0,0 +1,16 @@
+# def count(word,letter):
+#     counter = 0
+#     for l in word:
+#         if l == letter:
+#             counter = counter + 1
+#     return counter
+# word = input("Enter a word: ")
+# letter = input("What letter count would you like to find?")
+# result = count(word,letter)
+# print(result)
+# print("banana".count("a"))
+str = "X-DSPAM-Confidence: 0.8475"
+place = str.find(":")
+target = str[place+2:]
+answer = float(target)
+print(answer)
